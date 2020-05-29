14bffe8d870ca2171c2fe76291b5e667bfab201f
joshuamsje <49621706+joshuamsje@users.noreply.github.com>
2020-05-28 20:39:13 -0400

    Create ExternalSort.java

---
+++ ExternalSort.java
@@ -0,0 +1,50 @@
+package project3;
+/**
+ * On my honor:
+
+
+ * 
+ * - I have not used source code obtained from another student,
+ * or any other unauthorized source, either modified or
+ * unmodified.
+ * 
+ * -All source code and documentation used in my program is
+ * either my original work, or was derived by me from the
+ * source code published in the textbook for this course.
+ * 
+ * - I have not discussed coding details about this project with
+ * anyone other than my parter (in case of a joint
+ * submission), instructor, ACM/UPE tutors, or the TAs assigned
+ * to this course. I understand that I may discuss the concepts
+ * of this program with other students, and that another student
+ * may help me debug my program so long as neither of us writes
+ * anything during the discussion or modifies any computer file
+ * during the discussion. I have violated neither the spirit nor
+ * letter of this restriction.
+ * 
+ * --Kurt Karpin (kkarp9)
+ * --Joshua Mathew (josh827)
+ * 
+ * OS: Windows 10
+ */
+
+/**
+ * Main Java file for running project
+ * @author Kurt Karpin (kkarp9)
+ * @author Joshua Mathew (josh827)
+ *
+ * @version 2020.4.26
+ */
+public class ExternalSort {
+    //parser meant to just parse the bytes and put them into input buffers and output buffers which are byte arrays
+    
+    //just call the parser to handle everthing main method
+    /**
+     * main method for project run
+     * @param args input
+     */
+    public static void main(String args[])
+    {
+        Parser parse = new Parser(args[1]);
+    }
+}
